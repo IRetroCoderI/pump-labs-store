1ce982fa0ab52fe10a3092b9ecb9fa781c8773b3
Jesus Ramirez <jesusramirez730@gmail.com>
2024-05-23 19:28:10 -0700

    Initial setup with basic components and tailwind css integration. Added components such as Header, ProductsList, and example prices, will be changed later.

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className = "bg-blue-500 p-4">
+            <h1 className = "text-white text-2xl">Pump-Labs Bitches</h1>
+        </header>
+    );
+}
