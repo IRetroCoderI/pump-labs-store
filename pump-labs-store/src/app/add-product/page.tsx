9635640705422822c6be0c0bd6c0cf4ebe26edeb
Jesus Ramirez <jesusramirez730@gmail.com>
2024-06-19 15:49:32 -0700

    going to try database form when on main pc, not sure of error yet

--- pump-labs-store/src/app/add-product/page.tsx
+++ pump-labs-store/src/app/add-product/page.tsx
@@ -1,3 +1,4 @@
+import { redirect } from "next/navigation";
 import { prisma } from "../lib/db/prisma";
 
 export const metadata = {
@@ -6,10 +7,20 @@ export const metadata = {
 
 async function addProduct(formData: FormData) {
     "use server";
+    const name = formData.get("name")?.toString();
+    const description = formData.get("description")?.toString();
+    const imageUrl = formData.get("imageUrl")?.toString();
+    const price = Number(formData.get("price") || 0);
+
+    if (!name || !description || !imageUrl || !price) {
+        throw Error("Missing Required Fields");
+    }
 
     await prisma.product.create({
-        
-    })
+        data: {name, description, imageUrl, price},
+    });
+
+    redirect("/");
 }
 
 export default function AddProductPage() {
